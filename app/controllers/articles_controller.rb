d9e846ae1b16cf485541f11c219d58bd085d3730
powlen70 <andrew@decisely.com>
2020-04-07 15:32:38 -0600

    destroy routes and functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,4 +36,11 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      flash[:notice] = "Article was successfully removed."
+      redirect_to articles_path
+  end
+
 end
