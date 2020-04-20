f2ee49d0205e7a1eac3e619afeb3abfc9124cb35
powlen70 <andrew@decisely.com>
2020-04-20 16:11:27 -0600

    added user and user model

---
+++ db/migrate/20200418201645_add_columns.rb
@@ -0,0 +1,8 @@
+class AddColumns < ActiveRecord::Migration[6.0]
+  def change
+  	add_column :users, :username, :string
+  	add_column :users, :email, :string
+		add_column :users, :created_at, :datetime
+	  add_column :users, :updated_at, :datetime
+  end
+end
