7d6d89e1fa7eed84bfbfc455ea5f049edd366fc9
powlen70 <andrew@decisely.com>
2020-04-21 09:44:53 -0600

    added relationship from user to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_18_201645) do
+ActiveRecord::Schema.define(version: 2020_04_21_152758) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
