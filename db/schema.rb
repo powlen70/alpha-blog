950be1dfcc1ce2ba6a54574dfb86e06e3505af8d
powlen70 <andrew@decisely.com>
2020-04-22 07:49:15 -0600

    added BCrypt to the gemlock

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_152758) do
+ActiveRecord::Schema.define(version: 2020_04_21_211249) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_04_21_152758) do
     t.string "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "password_digest"
   end
 
 end
