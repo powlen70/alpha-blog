93b86c01bc31dcb0cc89facb584bd813a294ba8f
powlen70 <andrew@decisely.com>
2020-05-17 22:55:56 -0600

    category integrations testing

--- test/integration/create_categories_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CreateCategoriesTest < ActionDispatch::IntegrationTest
-
-	test "get new category form and create category" do
-		get new_category_path
-		
-	end
-end 
